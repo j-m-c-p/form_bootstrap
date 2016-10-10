cd18b51ba2e992ea860a0287d74e13245a8106ec
j-m-c-p <jhonatancubi@gmail.com>
2016-10-10 12:04:47 -0500

    Create formulario.php
    ejemplo sencillo de un formulario con bootstrap

---
+++ formulario.php
@@ -0,0 +1,60 @@
+<html>
+  <head>
+	   <title>Formulario</title>
+	
+      <link rel="stylesheet"  href="bootstrap/css/bootstrap.css">
+  
+  </head>
+    <body>
+	   <div class="container" >
+      <div class="row"></div>
+      <br></br>
+      <br></br>
+       <div class="row">
+        <div class="col-xs-12 col-md-2 "></div>
+        <div class="col-xs-12 col-md-8 well">
+          <form role="form">
+              <div class="form-group">
+                <label >Documento</label>
+                <input type="text" class="form-control" id="Documento" 
+                placeholder="Documento">
+              </div>
+                <div class="form-group">
+                  <label >Nombres</label>
+                  <input type="text" class="form-control" id="Nombres" 
+                  placeholder="Nombres">
+                </div>
+                <div class="form-group">
+                  <label >Apellidos</label>
+                  <input type="text" class="form-control" id="Apellidos"
+                  placeholder="Apellidos">
+                </div>
+                <div class="form-group">
+                  <label >Fecha de nacimiento</label>
+                  <input type="text" class="form-control" id="Fecha de nacimiento" 
+                     placeholder="Fecha de nacimiento">
+                </div>
+                <div class="form-group">
+                  <label >Celular</label>
+                  <input type="text" class="form-control" id="Celular"
+                  placeholder="Celular">
+                </div>
+                <div class="form-group">
+                  <label >Rh</label>
+                  <input type="text" class="form-control" id="Rh" 
+                  placeholder="Rh">
+                </div>
+           	  <center>
+                <button type="submit" class="btn btn-success">
+            	  <span class="glyphicon glyphicon-floppy-disk"> </span> Guardar</button>
+            	  <button type="submit" class="btn btn-success">
+            	  <span class="glyphicon glyphicon-remove-circle"> </span> Cancelar</button>
+              </center>
+          </form>
+        </div>
+        <div class="col-xs-12 col-md-2 "></div>
+        </div>
+    </div>
+  </body>
+</html>
+
